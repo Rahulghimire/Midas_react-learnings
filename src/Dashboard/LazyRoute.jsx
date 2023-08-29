a6b381a3f1463a3c9598c74d47df008879d94baf
Rahul Ghimire <ghimirerahul554@gmail.com>
2023-08-29 21:10:44 +0545

    Add nav

--- src/Dashboard/LazyRoute.jsx
+++ src/Dashboard/LazyRoute.jsx
@@ -1,4 +1,4 @@
 import { lazy } from "react";
 
 export const DashboardLayout = lazy(() => import("../Layout/DashboardLayout"));
-export const Registration = lazy(() => import("../pages/Register"));
+export const Registration = lazy(() => import("../pages/Registration"));
